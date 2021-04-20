30238d90e7c84236f48236520bb1f3a9caea09d8
clunven <cedrick.lunven@gmail.com>
2021-04-21 00:19:06 +0200

    update coode

--- demo-app/src/test/java/com/datastax/astra/Test03_WorkingWithRestApi.java
+++ demo-app/src/test/java/com/datastax/astra/Test03_WorkingWithRestApi.java
@@ -6,6 +6,9 @@ import static com.datastax.astra.DemoDataSet.clientId;
 import static com.datastax.astra.DemoDataSet.clientSecret;
 import static com.datastax.astra.DemoDataSet.ksName;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,11 +16,13 @@ import com.dstx.astra.sdk.AstraClient;
 
 import io.stargate.sdk.rest.domain.CreateTable;
 import io.stargate.sdk.rest.domain.Ordering;
+import io.stargate.sdk.rest.domain.SearchTableQuery;
 
 public class Test03_WorkingWithRestApi {
     
     public static final String ANSI_RESET           = "\u001B[0m";
     public static final String ANSI_GREEN           = "\u001B[32m";
+    
     private static AstraClient astraClient;
     
     // <CHANGE_ME>
@@ -33,6 +38,7 @@ public class Test03_WorkingWithRestApi {
         System.out.println(ANSI_GREEN + "[OK]" + ANSI_RESET + " - Astra Client Set");
     }
     
+    
     @Test
     public void listKeyspaces() {
         astraClient.apiRest().keyspaceNames()
@@ -42,9 +48,13 @@ public class Test03_WorkingWithRestApi {
     
     @Test
     public void createKeyspace() {
+        
         astraClient.apiDevops().createKeyspace(databaseId, "keyspace2");
+        
+        
         System.out.println(ANSI_GREEN + "[OK]" + ANSI_RESET + " - Keyspace created");
     }
+    
     
     @Test
     public void createTable() {
@@ -61,7 +71,21 @@ public class Test03_WorkingWithRestApi {
     
     @Test
     public void insertRow() {
-        
+        Map<String, Object> record = new HashMap<>();
+        record.put("firstname", "Mookie");
+        record.put("lastname", "Betts");
+        record.put("email", "mookie.betts@gmail.com");
+        record.put("color", "blue");
+        astraClient.apiRest().keyspace("keyspace2").table("users").upsert(record);
+        System.out.println(ANSI_GREEN + "[OK]" + ANSI_RESET + " - Row Inserted");
+    }
+    
+    @Test
+    public void readData() {
+        astraClient.apiRest().keyspace("keyspace2")
+                   .table("users").search(SearchTableQuery.builder().build())
+                   .getResults().stream().forEach(System.out::println);
+        System.out.println(ANSI_GREEN + "[OK]" + ANSI_RESET + " - List records");
     }
     
     
